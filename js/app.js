4a464e82d855cdca05a84ef56c19754204ee651d
AriyanShihab <devshihab111@gmail.com>
2022-08-22 01:32:26 +0600

    total cost of player calcualtion done

--- js/app.js
+++ js/app.js
@@ -1,8 +1,30 @@
-// appending player name on selected player container
-//  common function for get element by id
+//  common function: 01; for get element by id
 function getById(element) {
     return document.getElementById(element);
 }
+// common function: 02;
+// these function only take an id of input feild and it find and select that element with the help of
+// getById function and then return the value as a number, and the awsome part is it dose little bit of validation of input
+function getInputValueAsNumber(inputFeild) {
+    const element = getById(inputFeild);
+    const valueInString = element.value;
+    if (valueInString == "") {
+        alert("you can not enter an empty value");
+        return;
+    }
+    const valueInNumber = parseFloat(valueInString);
+    if (valueInNumber == NaN) {
+        alert("please enter a valid Number");
+        return;
+    }
+    return valueInNumber;
+}
+// common function: 03;
+// set inner text of element
+function setInnerTexr(elementToSet, whatToSet) {
+    getById(elementToSet).innerText = whatToSet;
+}
+// ====>>> task one: appending player name on selected player container
 // select button handeler
 const buttonParent = getById("buttonParent");
 buttonParent.addEventListener("click", function(e) {});
@@ -28,4 +50,12 @@ for (const btn of allButtons) {
     numberOfSelectedPLayer = selectedPlayerParent.children.length;
 }
 
-// total cost for player calculation
+// task two: total cost for player calculation
+
+const playerCostCalculateButton = getById("playerCostCalculateButton");
+playerCostCalculateButton.addEventListener("click", function() {
+    const eachPlayerCost = getInputValueAsNumber("costPerPlayer");
+    const selectedPlayer = getById("selectedPlayerParent").children.length;
+    const totalCostForPlayer = eachPlayerCost * selectedPlayer;
+    setInnerTexr("TotalCostForPlayer", totalCostForPlayer);
+});
